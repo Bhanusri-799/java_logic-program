f7a3c6f9311a9b071eacd200dbfc7443fa586b19
Bhanusri-799 <bhanusrirajulapati799@gmail.com>
2024-11-20 21:55:29 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+	class Student
+{
+public static void main (String args[])
+{
+int stid=92;
+String stname="Bhanusri";
+String stsec="MCA-B";
+System.out.println("Student Roll Number is :"+stid);
+System.out.println("Student Name is :"+stname);
+System.out.println("Student Group  is :"+stsec);
+}
+}
